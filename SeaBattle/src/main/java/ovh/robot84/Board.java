075cfdb3a21d766a9f601f3a350e94e76f4b11c0
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-11-29 00:25:06 +0100

    Cosmetic changes

--- SeaBattle/src/main/java/ovh/robot84/Board.java
+++ SeaBattle/src/main/java/ovh/robot84/Board.java
@@ -1,11 +1,27 @@
 package ovh.robot84;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Board extends JPanel {
-
-    private JPanel panelSzachowy;
+    //TODO zastosowano nowatorskie nadpisanie metody klasy JPanel() bez potrzeby tworzenia osobnej klasy dla tak nadpisanej motedy :) patrz i ucz sie :)
+    private JPanel panelSzachowy = new JPanel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            BufferedImage image = null;
+            try {
+                image = ImageIO.read(new File("C:\\Projects\\MavenSandbox\\src\\main\\resources\\img.jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            g.drawImage(image, 0, 0, null);
+        }
+    };
     private int verticalFields = 5;
     private int horizontallyFields = 5;
 
--- SeaBattle/src/test/java/ovh/robot84/GUITest.java
+++ SeaBattle/src/test/java/ovh/robot84/GUITest.java
@@ -1,8 +1,14 @@
 package ovh.robot84;
 
+import java.io.IOException;
+
 public class GUITest {
     public static void main(String[] args) {
-        MainForm mainForm = new MainForm();
+        try {
+            MainForm mainForm = new MainForm();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("Hello Swing!");
     }
 }
